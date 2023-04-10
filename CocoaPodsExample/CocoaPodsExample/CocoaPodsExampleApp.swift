7206ae77720f5e530358b1f64ad351041afa86fa
David Brunow <>
2023-04-10 06:37:54 -0500

    wip

---
+++ CocoaPodsExample/CocoaPodsExample/CocoaPodsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoaPodsExampleApp.swift
+//  CocoaPodsExample
+//
+//  Created by David Brunow on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CocoaPodsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
